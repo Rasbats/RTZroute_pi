d6b9f8fe839ecd8e2d5659c3e400eb2994f0c2e1
Rasbats <miker@members.swis.net>
2024-01-03 17:58:10 +0000

    Align SD

---
+++ Plugin.cmake
@@ -0,0 +1,105 @@
+# ~~~
+# Summary:      Local, non-generic plugin setup
+# Copyright (c) 2020-2021 Mike Rossiter
+# License:      GPLv3+
+# ~~~
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+
+# -------- Options ----------
+
+set(OCPN_TEST_REPO
+    "mike-rossiter/RTZroute-alpha"
+    CACHE STRING "Default repository for untagged builds"
+)
+
+
+#
+#
+# -------  Plugin setup --------
+#IF(WIN32)
+    FIND_LIBRARY(xerces-c_3 ${CMAKE_SOURCE_DIR}/src/xercesc)
+    TARGET_LINK_LIBRARIES(${PACKAGE_NAME} xerces-c_3)
+ENDIF(WIN32)
+set(PKG_NAME RTZroute_pi)
+set(PKG_VERSION  0.0.1)
+set(PKG_PRERELEASE "")  # Empty, or a tag like 'beta'
+
+set(DISPLAY_NAME RTZroute)    # Dialogs, installer artifacts, ...
+set(PLUGIN_API_NAME RTZroute) # As of GetCommonName() in plugin API
+set(PKG_SUMMARY "Simulate ship movements")
+set(PKG_DESCRIPTION [=[
+Simulates navigation of a vessel. Using the sail option and a current
+grib file for wind data, simulates how a sailing vessel might react in
+those conditions. Using 'Preferences' the simulator is able to record AIS
+data from itself. This can be replayed to simulate collision situations.
+]=])
+
+set(PKG_AUTHOR "Mike Rossiter")
+set(PKG_IS_OPEN_SOURCE "yes")
+set(PKG_HOMEPAGE https://github.com/Rasbats/RTZroute_pi)
+set(PKG_INFO_URL https://opencpn.org/OpenCPN/plugins/RTZroute.html)
+
+set(SRC
+        src/RTZroute_pi.h
+        src/RTZroute_pi.cpp
+        src/icons.h
+        src/icons.cpp
+        src/RTZroutegui.h
+        src/RTZroutegui.cpp
+        src/RTZroutegui_impl.cpp
+        src/RTZroutegui_impl.h
+        src/load-grammar-dom.cpp
+        src/load-grammar-dom.h
+        src/load-grammar-sax.cpp
+        src/load-grammar-sax.h
+        src/NavFunc.h
+        src/NavFunc.cpp
+)
+
+set(PKG_API_LIB api-18)  #  A dir in opencpn-libs/ e. g., api-17 or api-16
+
+macro(late_init)
+  # Perform initialization after the PACKAGE_NAME library, compilers
+  # and ocpn::api is available.
+
+  IF(WIN32)
+    SET(xercesc ${CMAKE_SOURCE_DIR}/src/xercesc)
+    SET(xercesc "${PARENT}.lib")
+
+    LINK_DIRECTORIES(${CMAKE_SOURCE_DIR}/src/xercesc)
+    INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/src/xercesc)
+    INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/src/xercesc/include)
+
+ENDIF(WIN32)
+
+IF(WIN32)
+    FIND_LIBRARY(xerces-c_3 ${CMAKE_SOURCE_DIR}/src/xercesc)
+    TARGET_LINK_LIBRARIES(${PACKAGE_NAME} xerces-c_3)
+ENDIF(WIN32)
+
+
+endmacro ()
+
+macro(add_plugin_libraries)
+  # Add libraries required by this plugin
+  add_subdirectory("${CMAKE_SOURCE_DIR}/opencpn-libs/tinyxml")
+  target_link_libraries(${PACKAGE_NAME} ocpn::tinyxml)
+
+  add_subdirectory("${CMAKE_SOURCE_DIR}/opencpn-libs/wxJSON")
+  target_link_libraries(${PACKAGE_NAME} ocpn::wxjson)
+
+  add_subdirectory("${CMAKE_SOURCE_DIR}/opencpn-libs/plugin_dc")
+  target_link_libraries(${PACKAGE_NAME} ocpn::plugin-dc)
+
+  add_subdirectory("${CMAKE_SOURCE_DIR}/opencpn-libs/jsoncpp")
+  target_link_libraries(${PACKAGE_NAME} ocpn::jsoncpp)
+
+  # The wxsvg library enables SVG overall in the plugin
+  add_subdirectory("${CMAKE_SOURCE_DIR}/opencpn-libs/wxsvg")
+  target_link_libraries(${PACKAGE_NAME} ocpn::wxsvg)
+endmacro ()
